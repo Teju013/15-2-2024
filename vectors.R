9d998c89febf3bbb6d9ce1ff3c33c4d09c066cdc
Teju013 <228w1a12c2@vrsec.ac.in>
2024-02-22 10:47:43 +0530

    vector operations

---
+++ vectors.R
@@ -0,0 +1,39 @@
+# Creating numeric vectors
+numeric_vector <- c(9 , 8 , 7 , 6 , 2)
+
+# Creating character vectors
+character_vector <- c("car", "bus", "bike")
+
+# Creating logical vectors
+logical_vector <- c(TRUE, FALSE, TRUE)
+
+numeric_vector[4]
+numeric_vector[3]
+
+vec1 <- c(5, 4, 4)
+vec2 <- c(2, 9, 3)
+vecadd<-vec1+vec2
+print(vecadd)
+
+vecmul<-vec1*vec2
+print(vecmul)
+
+vecsub<-vec1-vec2
+print(vecsub)
+
+equality<-vec1==vec2
+print(equality)
+
+vec<-c(20 , 30 , 40 , 50)
+print(vec)
+
+print(sum(vec))
+
+print(mean(vec))
+
+print(length(vec))
+
+print(vec1,vec2)
+
+concate<-c(vec1,vec2)
+print(concate)
